451086b0c083ca3d34380918d64ec946b44b0e84
Nicoolay <Rozhnov.nv1992@gmail.com>
2024-11-17 15:53:02 +0700

    Добавьте файлы проекта.

---
+++ Task_1 Вывод орграфа на консоль/Task_1 Вывод орграфа на консоль/Task_1 Вывод орграфа на консоль.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::ifstream input_file("input.txt");
+    int n;
+    input_file >> n;
+    if (!input_file) {
+        std::cerr << "Ошибка открытия файла!" << std::endl;
+        return 1;
+    }
+
+    // Создание двумерного массива для графа
+    int** arr = new int* [n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = new int[n];
+    }
+
+    // Чтение значений в двумерный массив из файла
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            input_file >> arr[i][j];
+        }
+    }
+    input_file.close();
+
+    // Вывод результата
+    std::cout << "Текстовый вид орграфа:\n";
+    
+    for (int i = 0; i < n; ++i) {
+        std::cout << i+1 << ":";    
+        int edge_count = 0; 
+        for (int j = 0; j < n; j++) {
+           
+            if (arr[i][j] == 1) {
+                std::cout << j + 1 << " ";
+                edge_count++;
+            }           
+        }
+
+        if (edge_count == 0) {
+            std::cout << "нет";  // Выводим "нет", если ни одной единицы не найдено
+        }
+        std::cout << std::endl;
+
+    }
+
+   
+
+    // Очистка выделенной памяти
+    for (int i = 0; i < n; i++) {
+        delete[] arr[i];
+    }
+    delete[] arr;
+   
+
+    return 0;
+}
---
+++ Task_2 Топологическая сортировка орграфа/Task_2 Топологическая сортировка орграфа/Task_2 Топологическая сортировка орграфа.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+void dfs(int** arr, int vertex, bool* visited,  int* order, int& orderIndex, int n) {
+    // Отмечаем текущую вершину как посещенную 
+    visited[vertex] = true;
+
+    // Проходим по всем вершинам, смежным с текущей
+    for (int i = 0; i < n; ++i) {
+        if (arr[vertex][i] == 1 && !visited[i]) {
+            dfs(arr,i, visited, order, orderIndex,n);
+        }      
+
+    }
+    order[orderIndex--] = vertex;
+   }
+int* top_sort(int** arr, bool* visited, int n) {
+    int* order = new int[n];
+    int orderIndex = n - 1;
+
+
+    for (int i = 0; i < n; ++i) {
+        if (!visited[i]) {
+            dfs(arr, i, visited, order, orderIndex, n);
+        }
+    }
+    return order;
+}
+
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::ifstream input_file("input.txt");
+    int n;
+    input_file >> n;
+    if (!input_file) {
+        std::cerr << "Ошибка открытия файла!" << std::endl;
+        return 1;
+    }
+
+    // Создание двумерного массива для графа
+    int** arr = new int* [n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = new int[n];
+    }
+
+    // Чтение значений в двумерный массив из файла
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            input_file >> arr[i][j];
+        }
+    }
+    input_file.close();
+    bool* visited = new bool[n];
+    // Инициализируем массив посещенных вершин значениями false
+    for (int i = 0; i < n; ++i) {
+        visited[i] = false;
+    }
+
+    int* order = top_sort(arr,visited, n);
+
+    std::cout << "Топологический порядок: ";
+    for (int i = 0; i < n; ++i) {
+        std::cout << (order[i]+1) << " ";
+    }
+
+    // Очистка выделенной памяти
+       delete[] order; 
+
+   for (int i = 0; i < n; i++) {
+        delete[] arr[i];
+    }
+    delete[] arr;
+
+
+    return 0;
+}
